6df7fcfe5f1a223c8d7ccc9e9fc6845e964dc29a
Dam Minh Hieu <daminhieu1703@gmail.com>
2022-08-18 12:45:15 +0700

    initial commit

--- send-mail.php
+++ send-mail.php
@@ -1,14 +1,14 @@
 <?php
-//  $to      = "daminhieu@gmail.com";
-//  $subject = "Chào".' '.$_POST['name'];
-//  $message = "Cảm ơn".' '.$_POST['name'].' '."đã liên hệ với tôi. Tôi sẽ cố gắng liên lạc lại với bạn sớm nhất";  
-//  $header  =  "From:".$_POST['email']."\r\n";
-//  $header .=  "Cc:".$_POST['email']."\r\n";
-//  mail ($to,$subject,$message,$header);
+ $to      = "daminhieu@gmail.com";
+ $subject = "Chào".' '.$_POST['name'];
+ $message = "Cảm ơn".' '.$_POST['name'].' '."đã liên hệ với tôi. Tôi sẽ cố gắng liên lạc lại với bạn sớm nhất";  
+ $header  =  "From:".$_POST['email']."\r\n";
+ $header .=  "Cc:".$_POST['email']."\r\n";
+ mail ($to,$subject,$message,$header);
 
-// $sub = $_POST['subject'].' - '.$_POST['name'];
-// $msg = $_POST['message'];
-// $rec = "daminhieu1703@gmail.com";
-//  mail ($rec, $sub, $msg);
+$sub = $_POST['subject'].' - '.$_POST['name'];
+$msg = $_POST['message'];
+$rec = "daminhieu1703@gmail.com";
+ mail ($rec, $sub, $msg);
  header('location:/daminhieu-profile/contact.html')
 ?>
